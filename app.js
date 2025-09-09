af53a5e522e7fa7ddabcf79c30fa2885367861c9
unknown <you@example.com>
2025-09-09 10:52:08 +0800

    Register and Login page is completed

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 8080;
+
+app.use(express.json());
+// app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -26,62 +30,67 @@ db.connect(err => {
 
 // ROUTES
 // Login page
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index');
 });
 
 // Register page
-app.get('/register', function(req, res) {
+app.get('/register', function (req, res) {
     res.render('register');
 });
 
+// Forgot password page
+app.get('/forgot-password', function (req, res) {
+    res.render('password');
+});
+
 // Admin Dashboard
-app.get('/admin/dashboard', function(req, res) {
-    res.render('admin', {render: {page: 'dashboard'}});
+app.get('/admin/dashboard', function (req, res) {
+    res.render('admin', { render: { page: 'dashboard' } });
 });
 
 // Admin User Management
-app.get('/admin/user-management/faculties', function(req, res) {
-    res.render('admin', {render: {page: 'faculty'}});
+app.get('/admin/user-management/faculties', function (req, res) {
+    res.render('admin', { render: { page: 'faculty' } });
 });
 
-app.get('/admin/user-management/students', function(req, res) {
-    res.render('admin', {render: {page: 'students'}});
+app.get('/admin/user-management/students', function (req, res) {
+    res.render('admin', { render: { page: 'students' } });
 });
 
-app.get('/admin/user-management/enrollment', function(req, res) {
-    res.render('admin', {render: {page: 'enrollment'}});
+app.get('/admin/user-management/enrollment', function (req, res) {
+    res.render('admin', { render: { page: 'enrollment' } });
 });
 
 // Admin Course Management
-app.get('/admin/course-management', function(req, res) {
-    res.render('admin', {render: {page: 'course-management'}});
+app.get('/admin/course-management', function (req, res) {
+    res.render('admin', { render: { page: 'course-management' } });
 });
 
 // Faculty Dashboard
-app.get('/faculty/dashboard', function(req, res) {
-    res.render('faculty', {render: {page: 'dashboard'}});
+app.get('/faculty/dashboard', function (req, res) {
+    res.render('faculty', { render: { page: 'dashboard' } });
 });
 
 // Faculty Course
-app.get('/faculty/course/modules', function(req, res) {
-    res.render('faculty', {render: {page: 'modules'}});
+app.get('/faculty/course/modules', function (req, res) {
+    res.render('faculty', { render: { page: 'modules' } });
 });
 
-app.get('/faculty/course/assignments', function(req, res) {
-    res.render('faculty', {render: {page: 'assignments'}});
+app.get('/faculty/course/assignments', function (req, res) {
+    res.render('faculty', { render: { page: 'assignments' } });
 });
 
-app.get('/faculty/course/tests', function(req, res) {
-    res.render('faculty', {render: {page: 'tests'}});
+app.get('/faculty/course/tests', function (req, res) {
+    res.render('faculty', { render: { page: 'tests' } });
 });
 
-app.get('/faculty/course/discussions', function(req, res) {
-    res.render('faculty', {render: {page: 'discussions'}});
+app.get('/faculty/course/discussions', function (req, res) {
+    res.render('faculty', { render: { page: 'discussions' } });
 });
 
-app.get('/faculty/communication/announcements', function(req, res) {
-    res.render('faculty', {render: {page: 'announcements'}});
+app.get('/faculty/communication/announcements', function (req, res) {
+    res.render('faculty', { render: { page: 'announcements' } });
 });
 
 // New routes here
@@ -91,4 +100,54 @@ app.get('/faculty/communication/announcements', function(req, res) {
 
 // <<<<<<<<<<<<<<<<<<
 
-app.listen(port, () => {console.log(`http://localhost:${port}`)});
+// API
+// >>>>>>>>>>>>>>>>>>
+
+app.post('/login', function (req, res) {
+
+    const validEmail = req.body.email;
+    const validPassword = req.body.password;
+
+    const myQuery = `SELECT * FROM users WHERE email = '${validEmail}'`;
+    
+    db.query(myQuery, function (err, row) {
+        if (err) throw err;
+        const hash = row[0].password;
+
+        const checkPassword = bcrypt.compareSync(validPassword, hash);
+
+        if (checkPassword) {
+            if (row[0].role_id === 1) {
+                res.send({'redirect': '/faculty/dashboard'});
+            } else {
+                res.send({'redirect': '/student/dashboard'});
+            }
+        } else {
+            res.send({'error': 'Invalid Credentials'});
+        }
+    });
+})
+
+app.post('/register', function (req, res) {
+
+    const validName = req.body.fullname;
+    const validSchoolId = req.body.school_id;
+    const validEmail = req.body.email;
+    const validPassword = req.body.password;
+
+    const hash = bcrypt.hashSync(validPassword, 10)
+
+    const myQuery = `INSERT INTO users 
+        (name, school_id, email, password, role_id) VALUES ("${validName}", "${validSchoolId}", "${validEmail}", "${hash}", 2)`;
+
+    db.query(myQuery, function (err, result) {
+        if (err) throw err;
+        console.log("result from database: ", result);
+    });
+
+    res.send({'redirect': '/'});
+})
+
+// <<<<<<<<<<<<<<<<<<
+
+app.listen(port, () => { console.log(`http://localhost:${port}`) });
